7b412540bc54cfd94064d6aa94e5c44053f8f020
kstawinski <stawinskidev@gmail.com>
2020-07-19 21:00:05 +0200

    Some styles for textarea and preview box

--- src/App.svelte
+++ src/App.svelte
@@ -2,15 +2,33 @@
 	import Helper from './components/Helper.svelte';
 </script>
 
-<main>
-	<textarea name="" id="" cols="30" rows="10" class="main__textarea main__textarea--a">
-	</textarea>
+<main class="main">
+	<textarea class="main__textarea main__textarea--field" autofocus></textarea>
+	<div class="main__textarea main__textarea--preview"></div>
 
-	<textarea name="" id="" cols="30" rows="10" class="main__textarea main__textarea--a">
-	</textarea>
-
-	<Helper/>
+	<Helper />
 </main>
 
 <style>
+.main {
+	display: grid;
+	grid-template-columns: repeat(2, 1fr);
+}
+
+.main__textarea {
+	padding: 40px;
+	font-size: 16px;
+	height: 100vh;
+}
+
+.main__textarea--field {
+	font-family: inherit;
+	border: 0;
+	outline: 0;
+	resize: none;
+}
+
+.main__textarea--preview {
+	background-color: #eaeaea;
+}
 </style>
